0008a6928955ac95711cb0568a5f14f7326c459a
zwyyy456 <zwyyy456@hotmail.com>
2023-06-28 20:04:32 +0800

    sleep

---
+++ user/sleep.c
@@ -0,0 +1,18 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(2, "Usage: sleep x...\n");
+        exit(1);
+    }
+    if (argc >= 3) {
+        fprintf(2, "two many arguments...\n");
+        exit(1);
+    }
+    int time = atoi(argv[1]);
+    printf("(nothing happen for a little while)\n");
+    sleep(time);
+    exit(0);
+}
