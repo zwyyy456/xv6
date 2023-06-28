4beefc46a225794da8b7481716027ce3dea4b637
zwyyy456 <zwyyy456@hotmail.com>
2023-06-28 20:02:32 +0800

    sleep

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/zwyyy/Documents/code/mit/xv6-labs-2021
+# Build directory: /home/zwyyy/Documents/code/mit/xv6-labs-2021/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
