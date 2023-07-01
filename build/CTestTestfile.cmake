c32357ebc7031932db35f4ab4604a9eb36f2c029
zwyyy456 <zwyyy456@hotmail.com>
2023-07-01 18:51:07 +0800

    fix runcmd

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zwyyy/Documents/code/mit/xv6-labs-2021
-# Build directory: /home/zwyyy/Documents/code/mit/xv6-labs-2021/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
